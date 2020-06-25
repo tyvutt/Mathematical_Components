f5ddb4b6cd6fe7cf0d6a2891432f339bf983b579
tyvutt <t160d011@gunma-u.ac.jp>
2020-06-25 23:17:30 +0900

    DAY 2

--- mathcomp_exer.v
+++ mathcomp_exer.v
@@ -1,3 +1,7 @@
+(*******    Mathematical Components    *******)
+(* Part One  : Basics for Formal Mathematics *)
+(* Chapter 1 : Functions and Computation     *)
+Section Chap1.
 From mathcomp Require Import all_ssreflect.
 Set Implicit Arguments.
 Unset Strict Implicit.
@@ -37,22 +41,22 @@ Eval compute in pred5 11.
 
 Definition three_patterns n :=
   match n with
-  u.+1.+1.+1.+1.+1 => u 
+    u.+1.+1.+1.+1.+1 => u 
   | v.+1 => v
   | 0 => n
   end.
-Eval compute in three_patterns 6.
+Eval compute in three_patterns 7.
 
 Definition same_bool b1 b2 :=
   match b1, b2 with
-  | true, true => true
+    true, true => true
   | false, false => true
   | _, _ => false
   end.
 
 Fixpoint addn n m :=
   match n with
-  | 0 => m
+    0 => m
   | p.+1 => (addn p m).+1 
   end.
 
@@ -72,3 +76,44 @@ Notation "[ ’seq’ E | i <- s ]" := (map (fun i => E) s).
 Eval compute in [seq i.+1 | i <- [:: 1; 2]].
 
 Inductive option A := None | Some (a : A).
+
+Print foldr.
+Print iota.
+Notation "\sum_ ( m <= i < n ) F" :=
+  (foldr (fun i a => F + a) 0 (iota m (n-m))).
+Eval compute in \sum_( 1 <= i < 3 ) (i * 2 - 1).
+Eval compute in \sum_( 1 <= i < 5 ) i.
+
+(* Exercise 1 The triple data type *)
+(* Exercise 2 Addition with iteration *)
+(* Exercise 3 Multiplication with iteration *)
+(* Exercise 4 Find the n-th element *)
+(* Exercise 5 List reversal *)
+(* ** Exercise 7 All words of size n *)
+End Chap1.
+
+(* Chapter 2 : First Steps in Formal Proofs *)
+Section Chap2.
+From mathcomp Require Import all_ssreflect.
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Check 3 = 3.
+Check 3 = 4.
+
+Locate "=".
+About eq.
+
+End Chap2.
+(* Chapter 3 : Type Theory *)
+(* Chapter 4 : First Steps in Formal Proofs *)
+
+
+(* Part Two  : Formalization Techniques *)
+(* Chapter 5 : Implicit Parameters *)
+(* Chapter 6 : Sub-Types *)
+(* Chapter 7 : Natural numbers *)
+
+
+(* Part Three : Synopsis *)
